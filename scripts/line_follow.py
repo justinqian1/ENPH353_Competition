92ae3472f6a8a7814983d9c63fd5069daade10b4
Justin Qian <jq3752@gmail.com>
2025-11-10 23:07:51 -0800

    added masks for everything for first surface

--- scripts/line_follow.py
+++ scripts/line_follow.py
@@ -6,45 +6,68 @@ from sensor_msgs.msg import Image
 from std_msgs.msg import Int32MultiArray
 from cv_bridge import CvBridge, CvBridgeError
 import numpy as np
+from scipy import ndimage
 
-def find_line(image,threshold=100):
-    """
-    @brief finds line near the robot
-    @param image, input image from camera
-    @param threshold, red channel value to determine what counts as the line
-    @return list of x and y coordinates of line closest to robot,
-            direction of the line further out in case the robot loses it,
-            length of the line at the target y coordinate
-            returns -1 for all coordinates if line not detected
-    """
-    '''
-    y_coord=790
-    line=np.where(image[y_coord,:,2]<threshold)[0]
-    length=len(line)
-    while length<5 and y_coord>400:
-        y_coord-=20
-        line=np.where(image[y_coord,:,2]<threshold)[0]
-        length=len(line)
+def find_features(image):
+    channel_b=image[:,:,0]
+    channel_g=image[:,:,1]
+    channel_r=image[:,:,2]
+    b_minus_g=channel_b-channel_g
+    b_minus_r=channel_b-channel_g
+    g_minus_r=channel_g-channel_r
+
+    line1_mask=(channel_b>245) & (channel_g>245) & (channel_r>245)
+    line2_mask=(channel_r>185) & (channel_r<220) & (channel_g>185) & (channel_g<220) & (channel_b>130) & (channel_b<170)
+    line_mask=line1_mask | line2_mask
+    sign_mask=(b_minus_g>90) & (b_minus_g<110) & (b_minus_r>90) & (b_minus_r<110)
+    red_line_mask=(channel_r>245) & (channel_b<10) & (channel_g<10)
+    pink_line_mask=(channel_r>245) & (channel_b>245) & (channel_g<10)
+    ped_mask=(b_minus_g>3) & (b_minus_g<13) & (g_minus_r>5) & (g_minus_r<15)
+    ped_mask[:380,:]=False
+    truck_mask=(channel_b>120) & (channel_b<240) & (b_minus_g==0) & (b_minus_r==0) & (g_minus_r==0)
+
+    features_mask=np.zeros(image.shape,dtype=np.uint8)
+    features_mask[520:,350:355,2]=90 # driving box 1
+    features_mask[520:,445:450,2]=90
+    features_mask[520:525,350:450,2]=90
+    features_mask[600:,180:185,2]=120 # driving box 2
+    features_mask[600:,615:620,2]=120
+    features_mask[600:605,180:620,2]=120
+    features_mask[line_mask]=255
+    features_mask[:,:,0][sign_mask]=255
+    features_mask[:,:,2][red_line_mask]=255
+    features_mask[:,:,0][pink_line_mask]=255
+    features_mask[:,:,2][pink_line_mask]=255
+    features_mask[ped_mask]=100
+    features_mask[truck_mask]=200
+    #cv2.imwrite('/tmp/frame.png',image)
+    cv2.imshow('camera feed', image)
+    cv2.imshow('line',features_mask)
+    cv2.waitKey(1)
+
+    # decide if line is directly in front of robot (need to turn)
+    line_in_front=np.sum(line_mask[520:,350:450])+np.sum(line_mask[600:,180:620]) 
+    line_left=np.where(line_mask[:,0])[0]
+    line_left_coord=line_left[-5] if len(line_left)>=5 else -1 # use -5 to avoid outliers/noise
+    line_right=np.where(line_mask[:,-1])[0]
+    line_right_coord=line_right[-5] if len(line_right)>=5 else -1
+    red_line_sz=np.sum(red_line_mask[:])
+    pink_line_sz=np.sum(pink_line_mask[:])
+    ped_sz=np.sum(ped_mask[:])
+    truck_sz=np.sum(truck_mask[:])
     
-    far_line=np.where(image[600,:,2]<threshold)[0]
-    if len(far_line) == 0:
-        dir = -1
-    else:
-        dir = (far_line[0]+far_line[-1])>800 # left = 0, right = 1
-
-    if length < 5:
-        return [-1,-1,-1,-1] # no line
-    else:
-        if length > 200:
-            if np.median(line) > 400:
-                x_coord=line[length//3]
-            else:
-                x_coord=line[2*length//3]
-        else:
-            x_coord=np.median(line).astype(int)
-        return [x_coord,y_coord,dir,length]
+    
+    sign_size=np.sum(sign_mask[:])
     '''
-    return None
+    if sign_size>2000: # enough of the sign is visible    
+        labeled, _ = ndimage.label(sign_mask)
+        largest=np.bincount(labeled.ravel())[1:].max()
+        print(largest)
+        #if largest>4000: # one sign; now check if all sides of sign are visible
+        # TODO
+    '''
+
+    return [line_in_front,line_left_coord,line_right_coord,sign_size,red_line_sz,pink_line_sz,ped_sz,truck_sz]
     
 class LineDetector:
     """
@@ -72,12 +95,10 @@ class LineDetector:
         except CvBridgeError as e:
             rospy.logerr(e)
             return
-        cv2.imwrite("/tmp/frame.png", cv_image)
-        cv2.imshow('camera feed',cv_image)
-        cv2.waitKey(1)
-        #msg = Int32MultiArray()
-        #msg.data = find_line(cv_image)
-        #self.result_pub.publish(msg)
+        find_features(cv_image)
+        msg = Int32MultiArray()
+        msg.data = find_features(cv_image)
+        self.result_pub.publish(msg)
 
 def main():
     rospy.init_node('line_detector', anonymous=True)
