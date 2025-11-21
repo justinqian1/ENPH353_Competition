491f95b5dbb53b35be7ad5f902ba77c253355aa1
ezraklukas <ezraklukas@gmail.com>
2025-11-21 08:48:57 -0800

    Testing manual preset partitions for character extraction

--- scripts/plate_detect.py
+++ scripts/plate_detect.py
@@ -15,12 +15,21 @@ GRAY_MASK = [10, -10, 10, -10, 10, -10]
 TEXT_MASK = [256, 30, 256, 30, 256, -256]
 MASK_DICT = {"b_g_upper":0, "b_g_lower":1, "b_r_upper":2,
              "b_r_lower":3, "g_r_upper":4, "g_r_lower":5}
-MIN_BLUE_COUNT = 4000
-MIN_PLATE_AREA = 3000
+MIN_BLUE_COUNT = 10000
+MIN_PLATE_AREA = 24000
 MIN_SIGN_COUNT = 100
 MIN_SIGN_AREA = 100
-OUTPUT_SHAPE = (400,200)
+OUTPUT_SHAPE = (800,400)
 COOL_DOWN_TIME = 5.0
+CHAR_WIDTH_PROP = 0.075
+CHAR_HEIGHT_PROP = 0.15
+BOT_ROW_Y = 268/400
+TOP_ROW_Y = 45/400
+BOT_ROW_X = 0.048
+TOP_ROW_X = 325/800
+CHARS_TOP_ROW = 7
+CHARS_BOT_ROW = 12
+
 
 class PlateDetector:
     """
@@ -30,6 +39,12 @@ class PlateDetector:
     Processes images from camera live feed and takes a picture once plate is found, to send over **ROSTOPIC**
     """
     def apply_mask(self, image, mask):
+        """
+        @brief applies and binarizes channel difference mask
+        @param image, the input image
+        @param mask, the difference mask to apply
+        @returns binarized applied channel difference mask.
+        """
         channel_b=image[:,:,0]
         channel_g=image[:,:,1]
         channel_r=image[:,:,2]
@@ -44,10 +59,19 @@ class PlateDetector:
 
         return analysis_mask
 
-    def extract_process_plate(self, frame, mask, min_count, min_area, output_shape=(400,200)):
+    def extract_process_plate(self, frame, mask, min_count, min_area, output_shape=OUTPUT_SHAPE):
+        """
+        @brief extracts and unskews largest masked quadrilateral in frame
+        @param frame the image to analyze
+        @param mask the mask to apply to find prospective figures
+        @param min_count the minimal pixel count required
+        """
         analysis_mask = self.apply_mask(frame, mask)
 
-        if np.count_nonzero(analysis_mask) < min_count:
+        pixel_count = np.count_nonzero(analysis_mask)
+        self.pixel_pub.publish(pixel_count)
+
+        if pixel_count < min_count:
             return None
 
         contours, _ = cv2.findContours(analysis_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
@@ -59,9 +83,12 @@ class PlateDetector:
 
         req_acc = 0.02 * cv2.arcLength(sign_cnt, True) # tells approxPolyDP maximal distance allowed between contour and simplified polygon.
         approx = cv2.approxPolyDP(sign_cnt, req_acc, True)
+        area = cv2.contourArea(approx)
+        
+        self.area_pub.publish(int(area))
 
         # If the shape of the largest contour isn't a rectangle, it's likely some of the border is off the screen, so we shouldn't trust that the text will be fully included.
-        if len(approx) != 4  or cv2.contourArea(approx) < min_area:
+        if len(approx) != 4  or area < min_area:
             return None
 
         corners = approx.reshape(4, 2).astype(np.float32)
@@ -93,7 +120,6 @@ class PlateDetector:
 
         return plate_rectified
 
-
     def scan_sign(self, mask, min_count, min_area):
         """
 
@@ -106,17 +132,50 @@ class PlateDetector:
                 return poss_plate
         return None
 
+    def isolate_letters(self, text_img):
+        contours, _ = cv2.findContours(text_img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
+        img_width, img_height = text_img.shape
+        w = int(img_width * CHAR_WIDTH_PROP)
+        h = int(img_height * CHAR_HEIGHT_PROP)
+        for cnt in contours:
+            x, y, _w, _h = cv2.boundingRect(cnt)
+            print("[top left](x,y): (" + str(x) + ", " + str(y) + "), (w, h): (" + str(_w) + ", " + str(_h) + ")")
+    
+            # Draw the rectangle on the original image
+            # (image, top-left corner, bottom-right corner, color, thickness)
+            cv2.rectangle(text_img, (x, y), (x + _w, y + _h), (255, 255, 0), 1)
+
+        return text_img
+
+    def text_predefined_boxes(self, text_img):
+        img_height, img_width = text_img.shape
+        print("Image width: " + str(img_width) + " Image height: " + str(img_height))
+        w = int(img_width * CHAR_WIDTH_PROP)
+        h = int(img_height * CHAR_HEIGHT_PROP)
+        x = int(img_width * TOP_ROW_X)
+        y = int(img_height * TOP_ROW_Y)
+        print("Top left of top row: (" + str(x) + ", " + str(y) + ").")
+        for char in range(CHARS_TOP_ROW):
+            cv2.rectangle(text_img, (x, y), (x + w, y + h), (255, 255, 0), 1)
+            x += w
+
+        x = int(img_width * BOT_ROW_X)
+        y = int(img_height * BOT_ROW_Y)
+        print("Top left of bottom row: (" + str(x) + ", " + str(y) + ").")
+        for char in range(CHARS_BOT_ROW):
+            cv2.rectangle(text_img, (x, y), (x + w, y + h), (255, 255, 0), 1)
+            x += w
+
+        return text_img
 
     def __init__(self):
         self.curr_plate = 1
         self.last_scan_time = time()
 
-        self.lpixel_pub = rospy.Publisher("/left_plate_pixel_count", Int32, queue_size=10)
-        self.cpixel_pub = rospy.Publisher("/centre_plate_pixel_count", Int32, queue_size=10)
-        self.rpixel_pub = rospy.Publisher("/right_plate_pixel_count", Int32, queue_size=10)
-        #self.area_pub = rospy.Publisher("/plate/largest_area", Int32, queue_size=10)
-        #self.cx_pub = rospy.Publisher("/plate/centroid_x", Float32, queue_size=10)
+        self.pixel_pub = rospy.Publisher("/plate/pixel_count", Int32, queue_size=10)
+        self.area_pub = rospy.Publisher("/plate/largest_area", Int32, queue_size=10)
+
         centre_image_topic = rospy.get_param("~centre_image_topic", "/B1/rrbot/camera1/image_raw")
         left_image_topic = rospy.get_param("~left_image_topic", "/B1/rrbot/lcam/image_raw")
         right_image_topic = rospy.get_param("~right_image_topic", "/B1/rrbot/rcam/image_raw")
@@ -144,7 +203,10 @@ class PlateDetector:
         if poss_plate is not None:
             poss_sign=self.extract_process_plate(poss_plate, GRAY_MASK, MIN_SIGN_COUNT, MIN_SIGN_AREA)
             if poss_sign is not None:                
-                cv2.imshow("Plate " + str(self.curr_plate), poss_sign)
+                extracted_text = self.apply_mask(poss_sign, TEXT_MASK)
+                #isolated_contours = self.isolate_letters(extracted_text)
+                isolated_contours = self.text_predefined_boxes(extracted_text)
+                cv2.imshow("Plate " + str(self.curr_plate), isolated_contours)
                 cv2.waitKey(3)
                 self.curr_plate += 1
                 self.last_scan_time = time()
